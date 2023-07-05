2add4d9cf0533dc20d078022a8882e1e49d0a036
kachDev7 <kachiamogu47@gmail.com>
2023-07-05 10:32:31 +0100

    updated receipt

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
 
       <main className="">
         kadxpress property here!
+        Goto <Link href="https://kadxpress.com" legacyBehavior>Home</Link>
       </main>
 
       
